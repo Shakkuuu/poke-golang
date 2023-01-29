6cf5970b1ff26823c8ec38037480db548a62a051
Shakkuuu <shakku0521@gmail.com>
2023-01-29 11:51:00 +0900

    敵モンスターの反撃と、発動した技の表示

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 type Page struct {
@@ -32,6 +35,7 @@ var pika = &Poke{Name: "ピカチュー", HP: 10, ATK: 2, Waza: []WAZA{jyuman, a
 var rotto = &Poke{Name: "オーロット", HP: 30, ATK: 1, Waza: []WAZA{uddohoon, noroi}}
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	http.HandleFunc("/", viewhandler)
 	http.ListenAndServe(":8080", nil)
 }
@@ -41,6 +45,7 @@ func viewhandler(w http.ResponseWriter, r *http.Request) {
 	page = &Page{Pika: pika, Rotto: rotto}
 	switch r.Method {
 	case "GET":
+		// fmt.Printf("GET: %d", randomu())
 		tmpl, err := template.ParseFiles("index.html") // ParseFilesを使う
 		if err != nil {
 			panic(err)
@@ -51,14 +56,33 @@ func viewhandler(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 	case "POST":
+		// fmt.Printf("POST: %d", randomu())
 		jyu := r.FormValue("jyu")
 		aian := r.FormValue("aian")
 		switch {
 		case jyu != "":
-			page.Rotto.HP -= page.Pika.Waza[0].ATK
+			page.Rotto.HP -= page.Pika.Waza[0].ATK * page.Pika.ATK
+			fmt.Println("ピカチュウの10まんボルト!")
 		case aian != "":
-			page.Rotto.HP -= page.Pika.Waza[1].ATK
+			page.Rotto.HP -= page.Pika.Waza[1].ATK * page.Pika.ATK
+			fmt.Println("ピカチュウのアイアンテール!")
+		}
+		switch randomu() {
+		case 0:
+			page.Pika.HP -= page.Rotto.Waza[0].ATK * page.Rotto.ATK
+			fmt.Println("オーロットのウッドホーン!")
+		case 1:
+			page.Pika.HP -= page.Rotto.Waza[1].ATK * page.Rotto.ATK
+			fmt.Println("オーロットののろい!")
+		case 2:
+			fmt.Println("オーロットは攻撃してこなかった")
 		}
 		http.Redirect(w, r, "/", 301)
 	}
+}
+
+func randomu() int {
+	ran := rand.Intn(3)
+	// fmt.Printf("kan: %d", ran)
+	return ran
 }
